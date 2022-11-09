8c4313cd8287f232b6d40850991bdd22b0a894d8
Iqbal Ahamed <70326995+iqbalAhamed@users.noreply.github.com>
2022-11-10 02:09:24 +0530

    Coding Practice

--- PyBasicCodes.py
+++ PyBasicCodes.py
@@ -4,3 +4,11 @@
 """
 
 numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for num in numbers: print (num)
+
+
+# fruits = ["Apple", "Mango", "Banana", "Peach"]
+ 
+# for index, element in enumerate(fruits):
+#   print(index, ":", element)
