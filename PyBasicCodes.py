17adb905c7c7571a431a9b79d2ca48a98e869c21
Iqbal Ahamed <70326995+iqbalAhamed@users.noreply.github.com>
2022-11-10 15:54:27 +0530

    Work with for loop and if - elif conditions

--- PyBasicCodes.py
+++ PyBasicCodes.py
@@ -2,14 +2,19 @@
 """
 ******* loop through list
 """
+"""
 
+"""
 numbers = [12, 75, 150, 180, 145, 525, 50]
 
-for num in numbers:
-     print (num)
-
-
-# fruits = ["Apple", "Mango", "Banana", "Peach"]
+for num in numbers:  
  
-# for index, element in enumerate(fruits):
-#   print(index, ":", element)
+    if num % 5 == 0:  #The number must be divisible by five      
+        if num > 150  and num < 500 : #If the number is greater than 150, then skip it and move to the next number
+            continue                       
+        elif num > 500 : #If the number is greater than 500, then stop the loop
+            print ("Number is greater than 500, loop terminated") 
+            break
+        else:
+            print(num) 
+   
