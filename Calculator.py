83ef740fb0779046b84479201fee350e76761cbf
Iqbal Ahamed <70326995+iqbalAhamed@users.noreply.github.com>
2022-11-16 01:13:57 +0530

    Added assignment 2

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,17 @@
-# Build a simple calculator 
-"""****************************"""
+# # # # Build a simple calculator 
+# # # """****************************"""
+# # # #My name is Keyaan,i want to build a calculator
+
+# # # #Multiplication
+
+# # # firstNumbr = int(input("Enter fist number"))
+# # # secondNumber = int(input("Enter Second Number"))
+
+# # # print("Ans-",firstNumbr + secondNumber)
+
+# # work = input("what do you want to input?")
+
+# # print("You have input for ",work)
+
+# name = input("what is you name? ")
+# print("My name is", name)
